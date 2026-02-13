c673d1ef19a52ab2bcd56d1d71890cb3d793fd00
waveeeh <genesis.nav19@gmail.com>
2026-02-13 23:05:47 +0800

    Fix: import App with lowercase

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 ﻿import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./app";
 import "./style/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
